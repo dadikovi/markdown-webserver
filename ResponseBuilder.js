199c9ee2a906ac85e17beff0ad6f4ff2ebd6b7fa
dadikovi <dadikovi@gmail.com>
2018-03-11 22:07:01 +0100

    Refactored index.js

---
+++ ResponseBuilder.js
@@ -0,0 +1,95 @@
+var marked = require('marked');
+var TemplateEngine = require('./TemplateEngine');
+var ErrorHandler = require('./ErrorHandler');
+
+module.exports = class ResponseBuilder {
+    static get init() {
+        class Builder {
+            constructor(dirLoader) {
+                this.response = {};
+                this.dirLoader = dirLoader;
+                this.templEngine = new TemplateEngine();
+                this.ErrorHandler = ErrorHandler;
+                this.marked = marked;
+
+                this.templEngine.init();
+
+                return this;
+            }
+            /** 
+             * This should be called when a http request arrives. 
+             */
+            reset() {
+                this.dirLoader.reset();
+                return this;
+            }
+
+            /** 
+             * Adds site name to the response object.
+             */
+            addName() {
+                this.response.name = this.dirLoader.getMarkdownStructure().name;
+                return this;
+            }
+            /**
+             * Adds the content-part of site. Normally this a parsed markdown file, but also can be error message.
+             * @param {*} path - the path-part of URL.
+             */
+            addContent(path) {
+                if (path === "/" || path === "") {
+                    this.response.content = this.marked(this.ErrorHandler.getDefaultContent(this.dirLoader));
+                    return this;
+                } else if (path.startsWith("/")) {
+                    path = path.substr(1);
+                }
+
+                try {
+                    var content_raw = this.dirLoader.getContent(path);
+                } catch (e) {
+                    if (e.reason !== undefined && e.reason === "NOT_FOUND") {
+                        this.response.content = this.marked(this.ErrorHandler.getNotFoundContent(this.dirLoader));
+                        return this;
+                    }
+                }
+
+                if (content_raw === undefined || content_raw === null) {
+                    this.response.content = this.marked(this.ErrorHandler.getEmptyContent(this.dirLoader));
+                    return this;
+                }
+                this.response.content = this.marked(content_raw);
+                return this;
+            }
+
+            /** 
+             * Adds the copyright information to the response object.
+             * The information can be declared in `COPYRIGHT.md` user template.
+             */
+            addCopyRight() {
+                var copy = "";
+                try {
+                    copy_raw = this.dirLoader.getUserTemplate("COPYRIGHT.md");
+                    copy = this.marked(copy_raw.content);
+                } catch (e) { }
+
+                this.response.copyright = copy;
+                return this;
+            }
+
+            /** 
+             * Adds the left part of the screen (explorer menu).
+             */
+            addExplorer() {
+                this.response.explorer = this.templEngine.renderExplorer(this.dirLoader.getMarkdownStructure());
+                return this;
+            }
+
+            /** 
+             * Renders the response object to HTML source and returns it.
+             */
+            toHtml() {
+                return this.templEngine.renderMain(this.response);
+            }
+        }
+        return Builder;
+    }
+};
--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-const path = require('path');
-var JSON = require('JSON');
-var marked = require('marked');
+var path = require('path');
+var ResponseBuilder = require('./ResponseBuilder');
+var DirectoryLoader = require('./DirectoryLoader');
 
 var app = express();
-var DirectoryLoader = require('./DirectoryLoader');
-var TemplateEngine = require('./TemplateEngine');
-var ErrorHandler = require('./ErrorHandler');
-
-var dirLoader;
-var templEngine;
-
-dirLoader = new DirectoryLoader();
-templEngine = new TemplateEngine();
-
-templEngine.init();
-readDirectory();
+var dirLoader = new DirectoryLoader();
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -29,62 +18,21 @@ app.use(function (req, res, next) {
 app.use(express.static('resources'));
 
 app.get('/*', function (req, res) {
-    reset();
-    var content = getContent(req.path);
-    var responseHTML = getResponseHTML(content);
-    res.send(responseHTML);
+    res.send(
+        new ResponseBuilder
+            .init(dirLoader)
+            .reset()
+            .addName()
+            .addContent(req.path)
+            .addCopyRight()
+            .addExplorer()
+            .toHtml()
+    );
 })
 
 app.listen(80, function () {
     console.log('INFO - markdown-webserver is listening on port 80!');
 });
-
-function reset() {
-    dirLoader.reset();
-}
-
-function getContent(path) {
-    if (path === "/" || path === "") {
-        return marked(ErrorHandler.getDefaultContent(dirLoader));
-    } else if (path.startsWith("/")) {
-        path = path.substr(1);
-    }
-
-    try {
-        var content_raw = dirLoader.getContent(path);
-    } catch (e) {
-        if (e.reason !== undefined && e.reason === "NOT_FOUND") {
-            return marked(ErrorHandler.getNotFoundContent(dirLoader));
-        }
-    }
-
-    if (content_raw === undefined || content_raw === null) {
-        return marked(ErrorHandler.getEmptyContent(dirLoader));
-    }
-    return marked(content_raw);
-}
-
-function getResponseHTML(content = "") {
-    var markdownStructure = dirLoader.getMarkdownStructure();
-    var copyright = getCopyright();
-    var explorer = templEngine.renderExplorer(markdownStructure);
-    return templEngine.renderMain({
-        name: markdownStructure.name,
-        explorer: explorer,
-        content: content,
-        copyright: copyright
-    });
-}
-
-function getCopyright() {
-    var copy = "";
-    try {
-        copy_raw = dirLoader.getUserTemplate("COPYRIGHT.md");
-        copy = marked(copy_raw.content);
-    } catch (e) {}
-
-    return copy;
-}
 
 function readDirectory() {
     var rootPath = parseRootPath()
@@ -119,3 +67,5 @@ function parseRootPath() {
         return rootPath;
     }
 }
+
+readDirectory();
